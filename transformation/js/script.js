33223ba78586f759a00c09aa1d540afa4418afdd
jfabes <89756328+jfabes@users.noreply.github.com>
2024-03-21 10:46:33 -0400

    Add files via upload

---
+++ transformation/js/script.js
@@ -0,0 +1,5 @@
+function transform() {
+  document.querySelector(".button").style.display = "none";
+  document.querySelector(".container").innerHTML +=
+    '<div class="transformed">Transformed!</div>';
+}
